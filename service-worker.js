0a5fed914807b83fe7f3f414fc60dc678a59d23b
mswphoto <mswphoto@naver.com>
2026-07-25 22:42:01 +0900

    Update service worker to version 3

--- service-worker.js
+++ service-worker.js
@@ -1,58 +1,77 @@
-const CACHE_NAME = "weightnote-v2";
+const CACHE_NAME = "weightnote-v3";
 
 const FILES = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./app.js",
-  "./manifest.json"
+    "./",
+    "./index.html",
+    "./style.css",
+    "./app.js",
+    "./manifest.json"
 ];
 
+
 // 새 버전 설치
+
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES);
-    })
-  );
 
-  self.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(FILES))
+    );
+
+    self.skipWaiting();
 });
 
-// 이전 캐시 삭제
+
+// 이전 버전 캐시 삭제
+
 self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames
-          .filter(name => name !== CACHE_NAME)
-          .map(name => caches.delete(name))
-      );
-    })
-  );
 
-  self.clients.claim();
+    event.waitUntil(
+
+        caches.keys().then(cacheNames => {
+
+            return Promise.all(
+
+                cacheNames
+                    .filter(name => name !== CACHE_NAME)
+                    .map(name => caches.delete(name))
+
+            );
+        })
+
+    );
+
+    self.clients.claim();
 });
 
-// 최신 파일 우선 사용
+
+// 최신 파일 우선
+
 self.addEventListener("fetch", event => {
 
-  if (event.request.method !== "GET") return;
+    if (event.request.method !== "GET") {
+        return;
+    }
 
-  event.respondWith(
-    fetch(event.request)
-      .then(response => {
+    event.respondWith(
 
-        const copy = response.clone();
+        fetch(event.request)
 
-        caches.open(CACHE_NAME).then(cache => {
-          cache.put(event.request, copy);
-        });
+            .then(response => {
 
-        return response;
-      })
-      .catch(() => {
-        return caches.match(event.request);
-      })
-  );
+                const copy = response.clone();
+
+                caches.open(CACHE_NAME)
+                    .then(cache => {
+                        cache.put(event.request, copy);
+                    });
+
+                return response;
+            })
+
+            .catch(() => {
+                return caches.match(event.request);
+            })
+
+    );
 });
